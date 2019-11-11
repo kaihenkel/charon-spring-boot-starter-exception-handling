318b137d1cb74f07ae117b9884fb00756164b392
Kai Henkel <henkel@develop4edu.de>
2019-11-11 08:19:28 +0100

    update error handling
    Mentioned within issue:
    https://github.com/mkopylec/charon-spring-boot-starter/issues/97

--- src/main/java/com/example/rest/ExampleApplication.java
+++ src/main/java/com/example/rest/ExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RestApplication {
+public class ExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RestApplication.class, args);
+		SpringApplication.run(ExampleApplication.class, args);
 	}
 
 }
--- src/main/java/com/example/rest/interseptor/TestForwardingInterseptor.java
+++ src/main/java/com/example/rest/interseptor/TestForwardingInterseptor.java
@@ -5,16 +5,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.util.UriComponentsBuilder;
 
 public class TestForwardingInterseptor implements RequestForwardingInterceptor {
     private static final Logger log = LoggerFactory.getLogger(TestForwardingInterseptor.class);
 
     @Override
     public HttpResponse forward(HttpRequest request, HttpRequestExecution execution) {
-        log.debug("Just Testing " + request.getURI());
+        if (request.getURI().getPath().startsWith("/testexample")) {
+            HttpResponse response = new HttpResponse(HttpStatus.FOUND);
+            response.getHeaders().setLocation(
+                UriComponentsBuilder.fromUri(request.getURI())
+                    .replacePath("/example")
+                    .build(true)
+                    .toUri()
+            );
+            return response;
+        }
         if (request.getURI().getPath().startsWith("/testerror")) {
             throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT);
         }
+
         return execution.execute(request);
     }
 
