c143f2f709595308f1177f15ad9c20e8a9efc3da
Kai Henkel <henkel@develop4edu.de>
2019-11-11 09:35:42 +0100

    update: Exception in custom filter

---
+++ src/main/java/com/example/rest/ExampleFilter.java
@@ -0,0 +1,25 @@
+package com.example.rest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.server.ResponseStatusException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class ExampleFilter extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+
+        if (request.getRequestURI().startsWith("/filter")) {
+            throw new ResponseStatusException(HttpStatus.I_AM_A_TEAPOT);
+        }
+        filterChain.doFilter(request, response);
+    }
+}
